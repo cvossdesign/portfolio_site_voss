4bb67f3213b032b7554c40c70915106f8e0b078d
cvossdesign <80014191+cvossdesign@users.noreply.github.com>
2021-04-15 18:20:44 -0500

    added responsive js dropdown menu to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Welcome to the Thunder Zone")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
